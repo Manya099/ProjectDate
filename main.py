7824092c2a503d3112a23e80302ec2b549b0366f
GreatCondemnator <81815559+GreatCondemnator@users.noreply.github.com>
2021-04-02 20:06:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+import telebot;
+bot = telebot.TeleBot('1650114540:AAEbzTKu60P0VE97xpMUGYOQqYzfyqP0mlg')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot.send_message(message.from_user.id, 'Ваше сообщение обработано, спасибо.')
+    print('From ' + str(message.from_user.id) + ' message "' + message.text + '"')
+
+bot.polling(none_stop = True, interval = 0)
