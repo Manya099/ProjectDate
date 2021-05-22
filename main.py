9c07011e15129c8b7719147f1b81016e3d49c0ba
GreatCondemnator <81815559+GreatCondemnator@users.noreply.github.com>
2021-05-22 14:08:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import telebot, database
+from telebot.apihelper import send_photo
 bot = telebot.TeleBot('1650114540:AAEbzTKu60P0VE97xpMUGYOQqYzfyqP0mlg')
+bot.delete_webhook()
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     bot.send_message(message.from_user.id, 'Ваше сообщение обработано, спасибо.')
     print('From ' + str(message.from_user.id) + ' message "' + message.text + '"')
-
-print(database.is_user_exists('3'))
-print(database.is_user_exists('4'))
+    bot.send_photo(message.from_user.id, 'AgACAgIAAxkBAAIBDWCo46zFg0uONMmE_lgsT1hAmRMHAAKUszEbpCxJSZQavgl64jg5Q3gUpC4AAwEAAwIAA20AA2tqAQABHwQ')
 
 bot.polling(none_stop = True, interval = 0)
